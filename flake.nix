6a1759f05da153f2f72c49b57b424563b31487b0
Lucas Franceschino <lucas@cryspen.com>
2025-04-09 08:51:39 +0200

    add new version

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@ fstar_v2025_01_07.url = "github:fstarlang/fstar/v2025.01.07";
 fstar_v2025_01_17.url = "github:fstarlang/fstar/v2025.01.17";
 fstar_v2025_02_06.url = "github:fstarlang/fstar/v2025.02.06";
 fstar_v2025_02_17.url = "github:fstarlang/fstar/v2025.02.17";
+fstar_v2025_03_25.url = "github:fstarlang/fstar/v2025.03.25";
 };
 outputs = { flake-utils, nixpkgs, ... }@inputs:
 flake-utils.lib.eachDefaultSystem (system:
@@ -11,6 +12,7 @@ fstar_v2025_01_07 = inputs.fstar_v2025_01_07.packages.${system}.default;
 fstar_v2025_01_17 = inputs.fstar_v2025_01_17.packages.${system}.default;
 fstar_v2025_02_06 = inputs.fstar_v2025_02_06.packages.${system}.default;
 fstar_v2025_02_17 = inputs.fstar_v2025_02_17.packages.${system}.default;
+fstar_v2025_03_25 = inputs.fstar_v2025_03_25.packages.${system}.default;
 }; }
 );
 }
